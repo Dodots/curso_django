14d23c1374436b84b96ce855b9dd9a8a3684b1f3
Douglas <doouglascarvalho92@gmail.com>
2020-10-27 10:28:42 -0300

    customizando o usuario

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -36,6 +36,7 @@ class UserManager(BaseUserManager):
 
         return self._create_user(email, password, **extra_fields)
 
+
 class UserManager(AbstractBaseUser, PermissionsMixin):
     """
     App base User classs.
